e9198ccc64780efee3d3539ff8aff51577b6136e
Dipto K Kundu <dkkundu00@gmail.com>
2022-11-12 20:23:56 +0600

    add post gress page

--- PERSONAL_WEBSITE/settings.py
+++ PERSONAL_WEBSITE/settings.py
@@ -164,16 +164,16 @@ LOGOUT_REDIRECT_URL = 'index'
 # Database --------------------------------------------------------------------
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': os.getenv('DB_CONFIG', DB_CONFIG)
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': os.getenv('DB_CONFIG', DB_CONFIG)
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation ---------------------------------------------------------
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- dj/bin/activate.fish
+++ dj/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/media/dipto/DK/Projects/MY-personal/personal-website/dj"
+set -gx VIRTUAL_ENV "/media/dipto/DK/Other/personal_website/dj"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
